f062ee251d1eb20f5af5616d840a1f48659df005
sk_codes <skyousufzai2@gmail.com>
2026-01-14 18:06:51 +0500

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -4,14 +4,23 @@
             input.value += number;
         }
 
-        const equal = () =>{
-            try{
-                input.value = eval(input.value);
-            }
-            catch(err){
-                alert("you entered the invalid input")
-            }
+       function equal() {
+    try {
+        const expression = input.value;
+
+        // Allow only numbers and math operators
+        if (!/^[0-9+\-*/%.() ]+$/.test(expression)) { // ^ this symbol means start string and $ end string
+            input.value = "Error";
+            return;
         }
+
+        const result = Function('"use strict"; return (' + expression + ')')();
+        input.value = result;
+    } catch {
+        input.value = "Error";
+    }
+}
+
 
 const clr = () => {
     input.value = "";
@@ -19,4 +28,5 @@ const clr = () => {
 
 const del = () => {
     input.value = input.value.slice(0,-1);
+
 }
